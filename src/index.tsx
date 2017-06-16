0a7c13f99cc0837c09bf2a4feb53566d617f8b3f
Mohd Asif <arshadasif0312@gmail.com>
2017-06-16 22:32:22 +0530

    remove redux related code
    could not get the container Card to work
    will get back to it later

--- src/index.tsx
+++ src/index.tsx
@@ -3,19 +3,8 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { StoreState } from './types/index';
-import { search } from './reducers/index';
-
-const store = createStore<StoreState>(search, {
-  searchText: 'Test',
-  items: [],
-});
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <App />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
